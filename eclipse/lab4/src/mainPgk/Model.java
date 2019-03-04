4d70c377474e689d1a460c6af6a4166ea7e75626
eakresho <eakresho@udel.edu>
2019-03-04 13:08:27 -0500

    modified some stuff

--- eclipse/lab4/src/mainPgk/Model.java
+++ eclipse/lab4/src/mainPgk/Model.java
@@ -8,3 +8,24 @@
  * provide direction
  * provide location
  **/
+public class Model{
+	public Model(int w, int h, int iw, int ih) {
+		
+	}
+	
+	public void updateLocationAndDirection() {
+		
+	}
+	
+	public int getX() {
+		return 0;
+	}
+	
+	public int getY() {
+		return 0;
+	}
+	public int getDirect() {
+		return 0;
+	}
+	
+}
--- eclipse/lab4/src/mainPgk/View.java
+++ eclipse/lab4/src/mainPgk/View.java
@@ -7,3 +7,28 @@
  * use proper images for direction
  * load images for all direction (an image should only be loaded once!!! why?)
  **/
+public class View{
+	int width;
+	int height;
+	int imageWidth;
+	int imageHeight;
+	
+	
+	public int getWidth(){
+		return 0;
+	}
+	public int getHeight(){
+		return 0;
+	}
+	public int getImageWidth(){
+		return 0;
+	}
+	public int getImageHeight(){
+		return 0;
+	}
+	
+	public void update(int x, int y, int dir){
+		
+	}
+	
+}
